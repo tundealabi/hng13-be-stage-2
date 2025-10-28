365929bb40e427d6165b5768f062a6f6d44b4fc5
Tunde Alabi <tundealabi4780@gmail.com>
2025-10-28 03:41:02 +0100

    fix: auto db create;

--- db/init.mjs
+++ db/init.mjs
@@ -9,6 +9,12 @@ const initDB = async () => {
     database: process.env.MYSQL_DATABASE,
   });
 
+  // Create database if not exists
+  await connection.query(
+    `CREATE DATABASE IF NOT EXISTS \`${process.env.MYSQL_DATABASE}\`;`
+  );
+  await connection.query(`USE \`${process.env.MYSQL_DATABASE}\`;`);
+
   const schema = fs.readFileSync('./db/schema.sql', 'utf-8');
   await connection.query(schema);
   await connection.end();
