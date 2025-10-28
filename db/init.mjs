c98411b19aafe33704dd46182b3eed462d7f5987
Tunde Alabi <tundealabi4780@gmail.com>
2025-10-28 03:55:36 +0100

    fix: auto db create;

--- db/init.mjs
+++ db/init.mjs
@@ -1,19 +1,29 @@
 import fs from 'fs';
 import mysql from 'mysql2/promise';
 
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+
 const initDB = async () => {
-  const connection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+  //Connect without specifying a DB
+  const initialConnection = await mysql.createConnection({
+    host: MYSQL_HOST,
+    user: MYSQL_USER,
+    password: MYSQL_PASSWORD,
   });
 
   // Create database if not exists
-  await connection.query(
-    `CREATE DATABASE IF NOT EXISTS \`${process.env.MYSQL_DATABASE}\`;`
+  await initialConnection.query(
+    `CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;`
   );
-  await connection.query(`USE \`${process.env.MYSQL_DATABASE}\`;`);
+  await initialConnection.end();
+
+  // Connect to the specific database to set up schema
+  const connection = await mysql.createConnection({
+    host: MYSQL_HOST,
+    user: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DATABASE,
+  });
 
   const schema = fs.readFileSync('./db/schema.sql', 'utf-8');
   await connection.query(schema);
